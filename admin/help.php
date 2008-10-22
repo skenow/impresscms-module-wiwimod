266635e917d994a665af947c7ff58bd056eebd78
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-10-22 20:28:04 +0000

    wiwimod - dev - created new file for use with help for all languages...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/wiwimod/trunk@5897 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ admin/help.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * About wiwimod, the wysiwyg wiki
+ * 
+ * @package Wiwimod
+ * @author Xavier JIMENEZ
+ * @author skenow <skenow@impresscms.org>
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @version $Id: about.php 5726 2008-10-16 16:00:36Z skenow $  
+ */
+/** Include the admin header for wiwimod */   
+include_once 'admin_header.php';
+
+xoops_cp_header();
+echo getAdminMenu (5,'help');
+echo "in development...";
+
+xoops_cp_footer();
+
+?>
