0d58008511a2062fd5093c92c24234ae76c25eca
Sina Asghari <stranger@impresscms.org>
2008-10-22 12:37:43 +0000

    a small update in Persian translation of wiwimod
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/wiwimod/trunk@5881 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/persian/install.php
+++ language/persian/install.php
@@ -10,23 +10,23 @@
  /** include the modinfo constants, so we don't have to define them again (specifically, _MI_WIWIMOD_WIWIHOME) */
  include_once 'modinfo.php';
 
- define('_MI_WIWIMOD_PROFILE1','Open Content');
- define('_MI_WIWIMOD_PROFILE2','Public Content');
- define('_MI_WIWIMOD_PROFILE3','Private Content'); 
- define('_MI_WIWIMOD_WIWIHOME_TITLE','Your Wiwi home page');
+ define('_MI_WIWIMOD_PROFILE1','„Õ Ê«&#1740; ¬“«œ');
+ define('_MI_WIWIMOD_PROFILE2','„Õ Ê«&#1740; ⁄„Ê„&#1740;');
+ define('_MI_WIWIMOD_PROFILE3','„Õ Ê«&#1740; Œ’Ê’&#1740;'); 
+ define('_MI_WIWIMOD_WIWIHOME_TITLE','’›ÕÂù&#1740; ¬€«“ Ê&#1740;Ê&#1740;');
 
  define('_MI_WIWIMOD_WIWIHOME_TEXT',
      "<p><table border=\'0\'>
      <tbody><tr><td>
-     <p>Welcome;<br />This is Wiwi\'s default home page. Feel free to edit and modify it. 
+     <p>ŒÊ‘ ¬„œ&#1740;œ∫<br />«&#1740;‰ ’›ÕÂù&#1740; Å&#1740;‘›—÷ Ê&#1740;Ê&#1740; „&#1740;ù»«‘œ. ‘„« „&#1740;ù «‰&#1740;œ »« Œ&#1740;«· —«Õ  «&#1740;‰ —« Ê&#1740;—«&#1740;‘ ò‰&#1740;œ Ê  €&#1740;&#1740;— œÂ&#1740;œ. 
      To create new pages, type in anywhere a page name and surround it with double square brackets ( [[ ). 
      When you save this page, the brackets will be replaced by a link to your new page.</p>
      <p>Check <a href=\'manual.html\' target=_blank>the manual</a> for an in depth view of editing features.</p></td>
      <td><img src=\'images/wiwilogo.gif\' /></td></tr></tbody></table></p>
      <p><table cellspacing=\'5\' cellpadding=\'5\' width=\'100%\' border=\'0\'>
-          <tbody><tr><td bgcolor=\'#e4e4e4\'>Pages index</td>
-          <td bgcolor=\'#e4e4e4\'>Recently modified pages</td></tr>
-          <tr><td bgcolor=\'#f6f6f6\'>&lt;[PageIndex]&gt;</td>
-          <td bgcolor=\'#f6f6f6\'>&lt;[RecentChanges]&gt;</td></tr></tbody></table>
+          <tbody><tr><td bgcolor=\'#e4e4e4\'>›Â—”  ’›Õ« </td>
+          <td bgcolor=\'#e4e4e4\'>¬Œ—&#1740;‰ ’›Õ«   €&#1740;&#1740;— &#1740;«› Â</td></tr>
+          <tr><td bgcolor=\'#f6f6f6\'>&lt;[›Â—” ]&gt;</td>
+          <td bgcolor=\'#f6f6f6\'>&lt;[¬Œ—&#1740;‰  €&#1740;&#1740;—« ]&gt;</td></tr></tbody></table>
      </p>");
 ?>
