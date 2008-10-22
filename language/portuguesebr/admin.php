2bad131a44deb3975a0850ff37c6d5dcd4c7ee00
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-10-22 16:09:47 +0000

    wiwimod - portuguesebr - step 2...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/wiwimod/trunk@5885 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/portuguesebr/admin.php
+++ language/portuguesebr/admin.php
@@ -1,91 +1,92 @@
 <?php
 /*
- * Module and admin language definition
+ * Module and admin language portuguese definition
  * 
- *	_BTN	: text within buttons or action links
+ * _BTN	: text within buttons or action links
  *  _COL	: column headers
  *  _TXT	: "verbose" text (probably within content)
  *  _FLD	: title of form elements
  *  _DESC	: description under the title for form elements
  *  _MSG	: messages, alerts ...
+ * Translation: GibaPhp - http://br.impresscms.org
  *
  */
 
 
-define('_AM_WIWI_ADMIN_TXT','Module administration');
-define('_AM_WIWI_PAGESFILTER_TXT','Show pages where');
-define('_AM_WIWI_LIKE_TXT','contains');
-define('_AM_WIWI_PROFILEIS_TXT','is');
-define('_AM_WIWI_ORDERBY_TXT',', ordered by');
-define('_AM_WIWI_LISTPAGES_RESULTS_TXT','results');
+define('_AM_WIWI_ADMIN_TXT','Administração');
+define('_AM_WIWI_PAGESFILTER_TXT','Onde Mostrar páginas');
+define('_AM_WIWI_LIKE_TXT','contém');
+define('_AM_WIWI_PROFILEIS_TXT','é');
+define('_AM_WIWI_ORDERBY_TXT',', ordenado por');
+define('_AM_WIWI_LISTPAGES_RESULTS_TXT','resultados');
 
 define('_AM_WIWI_SELECTACL_BOX','');
-define('_AM_WIWI_SELECTACL_TXT','modify a profile :');
-define('_AM_WIWI_CREATEACL_TXT','or create a new one :');
-define('_AM_WIWI_EDITACL_TXT','Profile info');
+define('_AM_WIWI_SELECTACL_TXT','modificar perfil :');
+define('_AM_WIWI_CREATEACL_TXT','ou criar um novo :');
+define('_AM_WIWI_EDITACL_TXT','Perfil info');
 define('_AM_WIWI_ACLHELP_TXT','
-	<p>Wiwi privileges are a set of named profiles describing xoops groups that enventually have read/write/administrate access to corresponding pages.</p>
+	<p>Os privilégios do Wiki é um conjunto de perfis que descrevem os grupos que têm enventualmente permissões para ler/gravar/administrar o acesso às páginas correspondentes.</p>
 	<ul>
-		<li>Writers&nbsp;can modify the current page, and create new pages with the same profile.</li>
-		<li>Administrators&nbsp;can change page profiles to whichever profiles they have "admin" privilege for.</li></ul>
-	<p>New pages default to their parent\'s profile.</p>
-	<p>Profiles also define who can read/post comments. This is useful to allow private comments on public pages ...</p>
-	<p>Note : Webmasters always have full privileges to Wiwi pages.</p>
+		<li>Escritores&nbsp;podem modificar a página atual, e criar novas páginas com o mesmo perfil.</li>
+		<li>Administradores&nbsp;podem modificar todas as páginas que desejam e também  qualquer perfil existente neste módulo.</li></ul>
+	<p>Definir padrão de Novas páginas para o perfil principal.</p>
+	<p>Os perfis também definem quem pode ler/enviar comentários. Isso é útil para permitir conteúdo privado e comentários em páginas públicas...</p>
+	<p>Nota : O Webmaster do site sempre têm todos privilégios nas páginas do Wiki.</p>
 	');
-define('_AM_WIWI_DELCONFIRMTITLE_TXT','DELETE PROFILE CONFIRMATION');
-define('_AM_WIWI_DELCONFIRM_TXT','You are about to delete a profile. Please confirm by checking the checkbox.');
-define('_AM_WIWI_DELREDIR_TXT','Choose a new profile for pages that were attached to this one.');
+define('_AM_WIWI_DELCONFIRMTITLE_TXT','CONFIRMAÇÃO PARA APAGAR O PERFIL');
+define('_AM_WIWI_DELCONFIRM_TXT','Você está prestes a apagar um perfil. Por favor, confirme marcando a caixa de seleção.');
+define('_AM_WIWI_DELREDIR_TXT','Escolha um novo perfil para AS páginas que foram anexadas a esta.');
 
-define('_AM_WIWI_LISTPAGES_BTN','<< Back to pages list');
-define('_AM_WIWI_CREATEACL_BTN','New');
-define('_AM_WIWI_EDITACL_SAVE_BTN','Save');
-define('_AM_WIWI_EDITACL_DELETE_BTN','Delete');
-define('_AM_WIWI_EDITACL_CANCEL_BTN','Cancel');
-define('_AM_WIWI_CLEANUPDB_BTN','Clean up the database');
+define('_AM_WIWI_LISTPAGES_BTN','<< Voltar à lista de páginas');
+define('_AM_WIWI_CREATEACL_BTN','Novo');
+define('_AM_WIWI_EDITACL_SAVE_BTN','Salvar');
+define('_AM_WIWI_EDITACL_DELETE_BTN','Apagar');
+define('_AM_WIWI_EDITACL_CANCEL_BTN','Cancelar');
+define('_AM_WIWI_CLEANUPDB_BTN','Limpar o banco de dados'); //Esta operação precisa ser verificada...
 
 
-define('_AM_WIWI_ACLNAME_FLD','Profile name');
-define('_AM_WIWI_ACLDESC_FLD','Profile Description');
-define('_AM_WIWI_READERS_FLD','Groups with read access');
-define('_AM_WIWI_WRITERS_FLD','Groups with write access');
-define('_AM_WIWI_ADMINISTRATORS_FLD','Groups with admin access');
-define('_AM_WIWI_COMMENTS_FLD','Who can read/post comments :');
-define('_AM_WIWI_HISTORY_FLD','Who can access page revisions history :');
-define('_AM_WIWI_DELREDIR_FLD','Replacement profile :');
+define('_AM_WIWI_ACLNAME_FLD','Nome do Perfil');
+define('_AM_WIWI_ACLDESC_FLD','Descrição do Perfil');
+define('_AM_WIWI_READERS_FLD','Grupos com acesso de <b>leitura</b>');
+define('_AM_WIWI_WRITERS_FLD','Grupos com acesso de <b>escrita</b>');
+define('_AM_WIWI_ADMINISTRATORS_FLD','Grupos com acesso <b>Administrativo</b>');
+define('_AM_WIWI_COMMENTS_FLD','Quem poderá ler e enviar comentários :');
+define('_AM_WIWI_HISTORY_FLD','Quem tem acesso nas páginas do histórico de revisões :');
+define('_AM_WIWI_DELREDIR_FLD','Substituição de perfil :');
 
-define('_AM_WIWI_SELECTACL_OPT','(select)');
-define('_AM_WIWI_READERS_OPT','Readers');
-define('_AM_WIWI_WRITERS_OPT','Writers');
-define('_AM_WIWI_ADMINISTRATORS_OPT','Administrators');
-define('_AM_WIWI_COMMENTS_NONE_OPT','(no comments)');
-define('_AM_WIWI_HISTORY_NONE_OPT','(no history)');
-define('_AM_WIWI_DELCONFIRM_OPT','YES, I want to delete this profile.');
+define('_AM_WIWI_SELECTACL_OPT','(selecione)');
+define('_AM_WIWI_READERS_OPT','Leitores');
+define('_AM_WIWI_WRITERS_OPT','Escritores');
+define('_AM_WIWI_ADMINISTRATORS_OPT','Administradores');
+define('_AM_WIWI_COMMENTS_NONE_OPT','(sem comentários)');
+define('_AM_WIWI_HISTORY_NONE_OPT','(sem histórico)');
+define('_AM_WIWI_DELCONFIRM_OPT','SIM, Quero apagar esse perfil.');
 
-define('_AM_WIWI_LISTPAGES_ALLPAGES_OPT','all pages');
-define('_AM_WIWI_LISTPAGES_KEYWORD_OPT','name');
-define('_AM_WIWI_LISTPAGES_TITLE_OPT','title');
-define('_AM_WIWI_LISTPAGES_BODY_OPT','content');
-define('_AM_WIWI_LISTPAGES_UID_OPT','last author');
-define('_AM_WIWI_LISTPAGES_PARENT_OPT','parent');
-define('_AM_WIWI_LISTPAGES_PRID_OPT','profile');
-define('_AM_WIWI_LISTPAGES_LASTMODIFIED_OPT','last modified');
-define('_AM_WIWI_LISTPAGES_ORDERDESC_OPT','descending');
-define('_AM_WIWI_LISTPAGES_ORDERASC_OPT','ascending');
+define('_AM_WIWI_LISTPAGES_ALLPAGES_OPT','todas páginas');
+define('_AM_WIWI_LISTPAGES_KEYWORD_OPT','nome');
+define('_AM_WIWI_LISTPAGES_TITLE_OPT','título');
+define('_AM_WIWI_LISTPAGES_BODY_OPT','conteúdo');
+define('_AM_WIWI_LISTPAGES_UID_OPT','último autor');
+define('_AM_WIWI_LISTPAGES_PARENT_OPT','principal');
+define('_AM_WIWI_LISTPAGES_PRID_OPT','perfil');
+define('_AM_WIWI_LISTPAGES_LASTMODIFIED_OPT','última modificação');
+define('_AM_WIWI_LISTPAGES_ORDERDESC_OPT','descendente');
+define('_AM_WIWI_LISTPAGES_ORDERASC_OPT','ascendente');
 
-define('_AM_WIWI_LISTPAGE_NAV','browse pages');
-define('_AM_WIWI_HISTORY_NAV','history');
-define('_AM_WIWI_ACLADMIN_NAV','privileges (ACL)');
-define("_AM_WIWI_BLOCKSNGROUPS_NAV", "blocks and groups");
+define('_AM_WIWI_LISTPAGE_NAV','navegar nas páginas');
+define('_AM_WIWI_HISTORY_NAV','histórico');
+define('_AM_WIWI_ACLADMIN_NAV','privilégios (ACL)');
+define("_AM_WIWI_BLOCKSNGROUPS_NAV", "blocos e grupos");
 
-define('_AM_WIWI_NOPAGESPECIFIED_MSG','Please select a page');
-define('_AM_WIWI_CONFIRMDEL_MSG','Do you really want to delete this Wiwi page?');
-define('_AM_WIWI_CONFIRMFIX_MSG','Do you really want to fix this Wiwi page?');
-define('_AM_WIWI_CONFIRMCLEAN_MSG','Do you really want to clean up the database?');
-define('_AM_WIWI_PRFSAVESUCCESS_MSG','Profile successfully saved on database');
-define('_AM_WIWI_PRFSAVEFAILED_MSG','Error while storing profile. Database NOT updated');
-define('_AM_WIWI_ERRDELETE_MSG','Error : unable to delete profile');
-define('_AM_WIWI_PRFDELSUCCESS_MSG','Profile deleted from the database.');
-define('_AM_WIWI_PRFDELFAILED_MSG','Error while deleting profile. Database NOT updated');
-define('_AM_WIWI_SYS_CFG','System Configuration');
+define('_AM_WIWI_NOPAGESPECIFIED_MSG','Escolha uma página');
+define('_AM_WIWI_CONFIRMDEL_MSG','Você realmente deseja excluir esta página do Wiki?');
+define('_AM_WIWI_CONFIRMFIX_MSG','Você realmente deseja corrigir esta página do Wiki?');
+define('_AM_WIWI_CONFIRMCLEAN_MSG','Você realmente deseja limpar o banco de dados? (CUIDADO)');
+define('_AM_WIWI_PRFSAVESUCCESS_MSG','Perfil gravado no banco com sucesso');
+define('_AM_WIWI_PRFSAVEFAILED_MSG','Erro ao armazenar o perfil. Base de dados não foi atualizada');
+define('_AM_WIWI_ERRDELETE_MSG','Erro: não foi possível apagar o perfil');
+define('_AM_WIWI_PRFDELSUCCESS_MSG','Perfil excluído do banco de dados.');
+define('_AM_WIWI_PRFDELFAILED_MSG','Erro ao excluir o perfil. Base de dados não foi atualizada');
+define('_AM_WIWI_SYS_CFG','Configuração do sistema');
 
 ?>
