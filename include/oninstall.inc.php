a526b5eaab249dc433f3294011432345e6038ec8
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-10-22 20:30:41 +0000

    wiwimod - dev - temporary fix... please change this code...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/wiwimod/trunk@5899 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- include/oninstall.inc.php
+++ include/oninstall.inc.php
@@ -10,19 +10,20 @@
  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
  * @version $Id$  
  */
- if (!defined("XOOPS_ROOT_PATH") && !defined('ICMS_ROOT_PATH')) die('Root path not defined');
+if (!defined("XOOPS_ROOT_PATH") && !defined('ICMS_ROOT_PATH')) die('Root path not defined');
  
 function xoops_module_install_wiwimod (){ 
- $mydir = dirname(dirname(__FILE__));
+// $mydir = dirname(dirname(__FILE__));
+ $mydirInstall = dirname(dirname(__FILE__));
 
- if (@file_exists($mydir.'/language/'.$xoopsConfig['language'].'/install.php')){
-      include $mydir.'/language/'.$xoopsConfig['language'].'/install.php';
+ if (@file_exists($mydirInstall.'/language/'.$xoopsConfig['language'].'/install.php')){
+      include $mydirInstall.'/language/'.$xoopsConfig['language'].'/install.php';
  } else {
-      include $mydir.'/language/english/install.php';
+      include $mydirInstall.'/language/english/install.php';
  }
 
 /* Add default profiles */ 
- include_once $mydir.'/class/wiwiProfile.class.php';
+ include_once $mydirInstall.'/class/wiwiProfile.class.php';
  $prof = new WiwiProfile();
  $prof->name = _MI_WIWIMOD_PROFILE1;
  $prof->prid = 0;
@@ -54,7 +55,7 @@ function xoops_module_install_wiwimod (){
  $prof->updateModuleConfig();
  
 /* add the default home page here */
-  include_once $mydir.'/class/wiwiRevision.class.php';
+  include_once $mydirInstall.'/class/wiwiRevision.class.php';
   $page = new WiwiRevision();
   $page->keyword = _MI_WIWIMOD_WIWIHOME;
   $page->title = _MI_WIWIMOD_WIWIHOME_TITLE;
@@ -72,6 +73,6 @@ function xoops_module_install_wiwimod (){
   return true;
  } 
 /* This will create a function with a name based on the installation directory */
- $myfunc = "function xoops_module_install_".basename(dirname(dirname(__FILE__)))."() { return xoops_module_install_wiwimod();}";
+ $myfunc = "function xoops_module_install_".$mydirInstall."() { return xoops_module_install_wiwimod();}";
  eval($myfunc);
 ?>
