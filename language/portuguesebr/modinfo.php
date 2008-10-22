57c57e73927dfe4306956e9f1f4be7a6b2acf952
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-10-22 17:02:53 +0000

    wiwimod - portuguesebr - step 6... Finalized initial translation. Starting a complete review and running tests with the current version of impressCMS....
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/wiwimod/trunk@5890 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/portuguesebr/modinfo.php
+++ language/portuguesebr/modinfo.php
@@ -1,64 +1,65 @@
 <?php
 // $Id: modinfo.php 5810 2008-10-19 03:35:40Z skenow $
 // Module Info
-
+// Translation: GibaPhp - http://br.impresscms.org
+ 
 // The name of this module
 define('_MI_WIWIMOD_NAME','Wiwi');
 
 // A brief description of this module
-define('_MI_WIWIMOD_DESC','A wysiwyg wiki tool.');
+define('_MI_WIWIMOD_DESC','Uma Ferramenta Visual de Wiki.');
 
 // Admin menu
-define('_MI_WIWIMOD_ADMENU1','Pages');
-define('_MI_WIWIMOD_ADMENU2','Privileges');
-define('_MI_WIWIMOD_ADMENU3','Blocks/Groups');
-define('_MI_WIWIMOD_ADMENU4','About...');
+define('_MI_WIWIMOD_ADMENU1','Páginas');
+define('_MI_WIWIMOD_ADMENU2','Privilégios');
+define('_MI_WIWIMOD_ADMENU3','Blocos/Grupos');
+define('_MI_WIWIMOD_ADMENU4','Sobre...');
 
 // Admin options
-define('_MI_WIWIMOD_EDITOR','Which editor should Wiwi use');
+define('_MI_WIWIMOD_EDITOR','Editor, que deve utilizar no Wiki');
 define('_MI_WIWIMOD_EDITOR_DESC','');
-define('_MI_WIWIMOD_DEFAULTPROFILE','Default profile');
+define('_MI_WIWIMOD_DEFAULTPROFILE','Perfil Padrão');
 // Default profile description was added in re-release, see below
 
-define('_MI_WIWIMOD_ALLOWPDF','Show PDF button on pages ?');
-define('_MI_WIWIMOD_ALLOWPDF_DESC','PDF generation from HTML pages is still at experimental level.');
+define('_MI_WIWIMOD_ALLOWPDF','Mostrar o botão para gerar páginas em PDF ?');
+define('_MI_WIWIMOD_ALLOWPDF_DESC','Gerar PDF baseado no conteúdo HTML das páginas ainda está em um nível experimental.');
 
-define('_MI_WIWIMOD_SHOWTITLES','Show page titles instead of page name');
-define('_MI_WIWIMOD_SHOWTITLES_DESC','Show page titles instead of page names in wiwilinks');
+define('_MI_WIWIMOD_SHOWTITLES','Mostrar títulos da página em vez do nome página');
+define('_MI_WIWIMOD_SHOWTITLES_DESC','Mostrar títulos da página, em vez de nomes na página tipo wikilinks');
 
-define('_MI_WIWIMOD_USECAMELCASE','Use CamelCase syntax');
-define('_MI_WIWIMOD_USECAMELCASE_DESC','Interprets CamelCase words as links to other wiki pages.');
+define('_MI_WIWIMOD_USECAMELCASE','Usar sintaxe CamelCase'); // Exemplo MostrarEstaPagina (veja que tem 3 letras maíusculas para representar o CamelCase) Isto faz a geração de um link automático para criar uma nova página.
+define('_MI_WIWIMOD_USECAMELCASE_DESC','Interpreta palavras CamelCase como links para outras páginas wiki.');
 
-define('_MI_WIWIMOD_XOOPSEDITOR','Choose a "XoopsEditor" supported editor');
-define('_MI_WIWIMOD_XOOPSEDITOR_DESC','Valid if XoopsEditor was chosen above');
+define('_MI_WIWIMOD_XOOPSEDITOR','Escolha um "Editor" suportado');
+define('_MI_WIWIMOD_XOOPSEDITOR_DESC','Válido somente se o Editor for escolhido acima');
 
 // Notification options
-define('_MI_WIWIMOD_PAGENOTIFYCAT_TITLE','Page');
-define('_MI_WIWIMOD_PAGENOTIFYCAT_DESC','Notifications that apply to the current page');
-define('_MI_WIWIMOD_PAGENOTIFY_TITLE','Page updated');
-define('_MI_WIWIMOD_PAGENOTIFY_CAPTION','Notify me when the current page is modified');
-define('_MI_WIWIMOD_PAGENOTIFY_DESC','Receive notification when any user updates the current page.');
-define('_MI_WIWIMOD_PAGENOTIFY_SUBJECT','[{X_SITENAME}] {X_MODULE} auto-notify : page updated');
+define('_MI_WIWIMOD_PAGENOTIFYCAT_TITLE','Página');
+define('_MI_WIWIMOD_PAGENOTIFYCAT_DESC','Notificações que serão aplicas à página atual');
+define('_MI_WIWIMOD_PAGENOTIFY_TITLE','Página atualizada');
+define('_MI_WIWIMOD_PAGENOTIFY_CAPTION','Avise-me quando a página atual for modificada');
+define('_MI_WIWIMOD_PAGENOTIFY_DESC','Deseja Receber uma notificação quando um usuário atualizar a página atual.');
+define('_MI_WIWIMOD_PAGENOTIFY_SUBJECT','[{X_SITENAME}] {X_MODULE} Notificação Automática: Página atualizada');
 /* Added in version 0.83 Re-release */
 define('_MI_WIWIMOD_GLOBALNOTIFYCAT_TITLE','Global');
-define('_MI_WIWIMOD_GLOBALNOTIFYCAT_DESC','Notifications that apply to the all pages');
-define('_MI_WIWIMOD_GLOBALNOTIFY_TITLE','Page updated');
-define('_MI_WIWIMOD_GLOBALNOTIFY_CAPTION','Notify me when any page is modified');
-define('_MI_WIWIMOD_GLOBALNOTIFY_DESC','Receive notification when any user updates any page.');
-define('_MI_WIWIMOD_GLOBALNOTIFY_SUBJECT','[{X_SITENAME}] {X_MODULE} auto-notify : page updated');
-define('_MI_WIWIMOD_TEMPLATE_VIEW_DESC','View Wiwi Page');
-define('_MI_WIWIMOD_TEMPLATE_EDIT_DESC','Edit Wiwi Page');
-define('_MI_WIWIMOD_TEMPLATE_HISTORY_DESC','View page history');
-define('_MI_WIWIMOD_TEMPLATE_PDF_DESC','WiwiMod - pdf');
-define('_MI_WIWIMOD_BLOCK_TOC_NAME','Wiwi TOC');
-define('_MI_WIWIMOD_BLOCK_TOC_DESC','Wiwi selected entry pages');
-define('_MI_WIWIMOD_BLOCK_RECENT_NAME','Wiwi Recent');
-define('_MI_WIWIMOD_BLOCK_RECENT_DESC','Wiwi recently modified');
+define('_MI_WIWIMOD_GLOBALNOTIFYCAT_DESC','Notificações que se aplicam a todas as páginas');
+define('_MI_WIWIMOD_GLOBALNOTIFY_TITLE','Página atualizada');
+define('_MI_WIWIMOD_GLOBALNOTIFY_CAPTION','Avise-me quando uma página for modificada');
+define('_MI_WIWIMOD_GLOBALNOTIFY_DESC','Receberá uma notificação quando um usuário atualizar uma página.');
+define('_MI_WIWIMOD_GLOBALNOTIFY_SUBJECT','[{X_SITENAME}] {X_MODULE} Notificação Automática : página atualizada');
+define('_MI_WIWIMOD_TEMPLATE_VIEW_DESC','Ver Página do Wiki');
+define('_MI_WIWIMOD_TEMPLATE_EDIT_DESC','Editar Página do Wiki');
+define('_MI_WIWIMOD_TEMPLATE_HISTORY_DESC','Ver página do Histórico');
+define('_MI_WIWIMOD_TEMPLATE_PDF_DESC','Wiki - pdf');
+define('_MI_WIWIMOD_BLOCK_TOC_NAME','Wiki TOC');
+define('_MI_WIWIMOD_BLOCK_TOC_DESC','Páginas escolhidas para iniciar o Wiki');
+define('_MI_WIWIMOD_BLOCK_RECENT_NAME','Wiki Recentes');
+define('_MI_WIWIMOD_BLOCK_RECENT_DESC','Modificações Recentes no Wiki');
 define('_MI_WIWIMOD_BLOCK_RELATED_NAME','WiwiSideContent');
-define('_MI_WIWIMOD_BLOCK_RELATED_DESC','Side block for extra content on Wiwi pages');
+define('_MI_WIWIMOD_BLOCK_RELATED_DESC','bloquear o conteúdo lateral extra sobre as páginas do Wiki');
 define('_MI_WIWIMOD_BLOCK_SHOWPAGE_NAME','WiwiShowPage');
-define('_MI_WIWIMOD_BLOCK_SHOWPAGE_DESC','Show a wiwi page');
-define('_MI_WIWIMOD_AUTHOR_WORD','<h4>About Wiwi 0.8.3</h4><br />Wiwi is GPL software ; visit Wiwi home page at <a href="http://www.zonatim.com/modules/wiwimod?page=WiwimodHomePage" target="_blank">www.zonatim.com</a> to support or get help.<br /><br />If you\'ve just migrated from an older Wiwi version (0.7.1 or less), please click here : <input type="button" value="UPGRADE" onclick="document.location.href=\'../update.php\';"><br /><br /><a href=\'../manual.html\' target=\'_blank\'>Read the Manual</a> and <a href=\'../ReadMe.txt\' target=\'_blank\'>release notes</a> to get started.');
-define('_MI_WIWIMOD_DEFAULTPROFILE_DESC','Default profile assigned to new pages');
+define('_MI_WIWIMOD_BLOCK_SHOWPAGE_DESC','Mostrar uma página do Wiki');
+define('_MI_WIWIMOD_AUTHOR_WORD','<h4>Sobre o Wiwi 0.8.3</h4><br />Wiwi é um software GPL ; visite a página inicial do Wiwi no <a href="http://www.zonatim.com/modules/wiwimod?page=WiwimodHomePage" target="_blank">www.zonatim.com</a> para suporte ou conseguir ajuda.<br /><br />Se você estiver migrando da versão (0.7.1 ou menor), favor clicar aqui : <input type="button" value="UPGRADE" onclick="document.location.href=\'../update.php\';"><br /><br /><a href=\'../manual.html\' target=\'_blank\'>Leia o Manual</a> e <a href=\'../ReadMe.txt\' target=\'_blank\'>notas sobre a atualização</a> para começar.');
+define('_MI_WIWIMOD_DEFAULTPROFILE_DESC','Perfil predefinido e atribuído para novas páginas');
 if (!defined('_MI_WIWIMOD_WIWIHOME'){define('_MI_WIWIMOD_WIWIHOME','WiwiHome');}
 ?>
