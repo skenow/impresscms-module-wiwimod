226d5c88d46a2b52aa31968ef459f81c78cb4682
Steve Keno <skenow@impresscms.org>
2008-10-27 16:05:24 +0000

    Declaring $xoopsConfig as a global in oninstall.inc.php so the install could complete in multi-languages
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/wiwimod/branches/wiwimod-0.83-revised@6064 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- include/oninstall.inc.php
+++ include/oninstall.inc.php
@@ -14,6 +14,7 @@
  
 function xoops_module_install_wiwimod (){ 
  $mydir = dirname(dirname(__FILE__));
+ global $xoopsConfig;
 
  if (@file_exists($mydir.'/language/'.$xoopsConfig['language'].'/install.php')){
       include $mydir.'/language/'.$xoopsConfig['language'].'/install.php';
